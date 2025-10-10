3ec7e09526652dfbe8157bf6af3f556b153be009
rayenandikaashari <2310631170043@student.unsika.ac.id>
2025-10-10 08:36:03 +0700

    Update brutal

--- lib/screens/thankyou_screen.dart
+++ lib/screens/thankyou_screen.dart
@@ -14,18 +14,36 @@ class ThankYouScreen extends StatelessWidget {
           children: [
             const Icon(Icons.star, color: Colors.yellow, size: 100),
             const SizedBox(height: 20),
-            const Text('TERIMA KASIH\nSUDAH MEMBELI:)', style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+            const Text(
+              'TERIMA KASIH\nSUDAH MEMBELI:)',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 40),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.white, foregroundColor: Colors.deepPurple, padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.deepPurple,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 15,
+                ),
+              ),
               onPressed: () {
                 Navigator.of(context).pushAndRemoveUntil(
                   MaterialPageRoute(builder: (ctx) => const WelcomeScreen()),
                   (Route<dynamic> route) => false,
                 );
               },
-              child: const Text('Belanja Lagi!', style: TextStyle(fontSize: 16)),
-            )
+              child: const Text(
+                'Belanja Lagi!',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ],
         ),
       ),
